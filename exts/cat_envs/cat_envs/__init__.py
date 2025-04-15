e8b971943746fa71546278a735b87c45b64aa805
Loukas Kordos <loukas.kordos@uni-tuebingen.de>
2025-04-15 16:56:59 +0200

    Fix dependency errors due to old isaac naming

--- exts/cat_envs/cat_envs/__init__.py
+++ exts/cat_envs/cat_envs/__init__.py
@@ -3,7 +3,7 @@ Python module serving as a project/extension template.
 """
 
 # Register Gym environments.
-from .tasks import *
+from isaaclab_tasks import *
 
 # Register UI extensions.
 from .ui_extension_example import *
