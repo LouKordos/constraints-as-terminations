dcb15e9df65dabb8d15f83070bcd5d73d2a720f0
Loukas Kordos <loukas.kordos@uni-tuebingen.de>
2025-04-16 09:31:30 +0200

    Formatting and more detailed comments

--- exts/cat_envs/cat_envs/tasks/utils/cat/curriculums.py
+++ exts/cat_envs/cat_envs/tasks/utils/cat/curriculums.py
@@ -17,19 +17,25 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from isaaclab.envs import ManagerBasedRLEnv
 
+# Curriculum function which gets called by Isaac Lab at certain interval. The intended effect is to slowly ramp up the maximum allowed termination probability
+# to prevent overwhelming the agent/policy with strict constraints early on. As the policy learns, the constraints are gradually enforced more and more by increasing
+# the maximum termination probability that a constraint violation can evoke.
 def modify_constraint_p(env: ManagerBasedRLEnv, env_ids: Sequence[int], term_name: str, num_steps: int, init_max_p: float,):
+    # Set to 0 if it's the first iteration
     if not hasattr(env.curriculum_manager, "constraints_curriculum"):
         env.curriculum_manager.constraints_curriculum = 0
 
-    step_cur = 1.0 / num_steps
+    # Expected step per function call, i.e. the max_p increment per function call.
+    current_curriculum_progress_step = 1.0 / num_steps
 
-    env.curriculum_manager.constraints_curriculum = min(env.curriculum_manager.constraints_curriculum + step_cur, 1.0)
+    env.curriculum_manager.constraints_curriculum = min(env.curriculum_manager.constraints_curriculum + current_curriculum_progress_step, 1.0)
 
     # Linearly interpolate the expected time for episode end: soft_p is the maximum
     # termination probability so it is an image of the expected time of death.
     T_start = 20
     T_end = 1 / init_max_p
-    init_max_p = 1 / (T_start + env.curriculum_manager.constraints_curriculum * (T_end - T_start))
+    interpolated_new_termination_time = T_start + env.curriculum_manager.constraints_curriculum * (T_end - T_start)
+    init_max_p = 1 / (interpolated_new_termination_time)
 
     # obtain term settings
     term_cfg = env.constraint_manager.get_term_cfg(term_name)
