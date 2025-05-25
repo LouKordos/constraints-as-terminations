cf741e418533dd453c92cf70a2af2b050308f1c7
Loukas Kordos <loukas.kordos@uni-tuebingen.de>
2025-05-25 16:30:50 +0200

    Add option for storing sim and PPO tensors on disk and hashing at specified interval for determinism troubleshooting

--- exts/cat_envs/cat_envs/tasks/utils/cleanrl/ppo.py
+++ exts/cat_envs/cat_envs/tasks/utils/cleanrl/ppo.py
@@ -160,6 +160,31 @@ def PPO(envs, ppo_cfg, run_path):
 
     BATCH_SIZE = int(NUM_ENVS * NUM_STEPS)
 
+    STORE_AND_HASH_TENSORS = True
+    if STORE_AND_HASH_TENSORS:
+        import zarr
+        from numcodecs import Blosc
+        import hashlib
+        snapshot_dir = os.path.join(run_path, "tensor_snapshots")
+        os.makedirs(snapshot_dir, exist_ok=True)
+        compress = Blosc(cname="zstd", clevel=3, shuffle=Blosc.BITSHUFFLE)
+        zroot = zarr.open_group(os.path.join(snapshot_dir, f"training_data_{os.path.basename(run_path)}.zarr"), mode="w")
+        signals = {
+            "actions"     : ((0, NUM_ENVS) + SINGLE_ACTION_SPACE        , 'f4'),
+            "rewards"     : ((0, NUM_ENVS)                              , 'f4'),
+            "obs"         : ((0, NUM_ENVS) + SINGLE_OBSERVATION_SPACE   , 'f4'),
+            "obs_rms"     : ((0, NUM_ENVS) + SINGLE_OBSERVATION_SPACE   , 'f4'),
+            "next_dones"  : ((0, NUM_ENVS)                              , 'f4'),
+            "returns"     : ((0,)                                       , 'f4'),
+        }
+        datasets = {}
+        for name, (shape, dtype) in signals.items():
+            datasets[name] = zroot.create_dataset(name, shape=shape, chunks=(128,) + shape[1:], dtype=dtype, compressor=compress, overwrite=True)
+
+        hashers = {name: hashlib.sha512() for name in datasets}
+        tensor_zarr_append_interval = 4 # Global training steps!
+        zroot.attrs["append_interval"] = tensor_zarr_append_interval
+
     agent = Agent(envs).to(device)
     optimizer = optim.Adam(agent.parameters(), lr=LEARNING_RATE, eps=1e-5)
 
@@ -209,6 +234,27 @@ def PPO(envs, ppo_cfg, run_path):
             # next_obs, rewards[step], next_done, timeouts, info = envs.step(torch.rand_like(action) * 0.6 - torch.ones_like(action) * 0.3)
             next_done = next_done.to(torch.float)
             next_obs = next_obs["policy"]
+
+            if STORE_AND_HASH_TENSORS:
+                # Store only first time step of each global iteration because otherwise it takes way too long.
+                # To be absolutely certain two runs are identical, remove the steps == 0 and decrease tensor_zarr_append_interval, at the cost of slower training.
+                if step == 0 and iteration % tensor_zarr_append_interval == 0:
+                    zarr_append_start = time.time()
+                    np_actions = action.detach().cpu().numpy()
+                    np_rewards = rewards[step].detach().cpu().numpy()
+                    np_obs = next_obs.detach().cpu().numpy()
+                    np_next_dones = next_done.detach().cpu().numpy()
+                    # print(f"actions zarr shape={datasets['actions'].shape}\tnp shape (pre_expand)={np_actions.shape}")
+                    # print(f"rewards zarr shape={datasets['rewards'].shape}\tnp shape (pre_expand)={np_rewards.shape}")
+                    # print(f"obs zarr shape={datasets['obs'].shape}\tnp shape (pre_expand)={np_obs.shape}")
+                    # print(f"next_dones zarr shape={datasets['next_dones'].shape}\tnp shape (pre_expand)={np_next_dones.shape}")
+
+                    datasets["actions"].append(np.expand_dims(np_actions, axis=0), axis=0)
+                    datasets["rewards"].append(np.expand_dims(np_rewards, axis=0), axis=0)
+                    datasets["obs"].append(np.expand_dims(np_obs, axis=0), axis=0)
+                    datasets["next_dones"].append(np.expand_dims(np_next_dones, axis=0), axis=0)
+                    print(f"iteration={iteration}\tAppending tensors to zarr file took {(time.time() - zarr_append_start):.4f} seconds")
+
             if torch.any(torch.isnan(next_obs)):
                 print("NAN IN OBSERVATION")
 
@@ -219,6 +265,11 @@ def PPO(envs, ppo_cfg, run_path):
 
             info["true_dones"] = timeouts
             next_obs = agent.obs_rms(next_obs)
+            if STORE_AND_HASH_TENSORS:
+                if step == 0 and iteration % tensor_zarr_append_interval == 0:
+                    np_obs_rms = next_obs.detach().cpu().numpy()
+                    datasets["obs_rms"].append(np.expand_dims(np_obs_rms, axis=0), axis=0)
+
             next_true_done = info["true_dones"].float()
             if "time_outs" in info:
                 if info["time_outs"].any():
@@ -264,6 +315,38 @@ def PPO(envs, ppo_cfg, run_path):
                 delta = (rewards[t] + GAMMA * nextvalues * nextnonterminal * true_nextnonterminal - values[t])
                 advantages[t] = lastgaelam = (delta + GAMMA * GAE_LAMBDA * nextnonterminal * true_nextnonterminal * lastgaelam)
             returns = advantages + values
+
+            if STORE_AND_HASH_TENSORS:
+                # Uses training iteration steps because it does not run in nested sim step loop
+                if iteration % tensor_zarr_append_interval == 0:
+                    np_returns = returns.detach().cpu().reshape(-1).numpy()
+                    print(f"returns zarr shape={datasets['returns'].shape}\tnp shape={np_returns.shape}")
+                    datasets["returns"].append(np_returns, axis=0)
+
+                    hash_update_start = time.time()
+                    for name, arr in [("actions", np_actions),
+                    ("rewards", np_rewards),
+                    ("obs", np_obs),
+                    ("obs_rms", np_obs_rms),
+                    ("next_dones", np_next_dones),
+                    ("returns", np_returns)]:
+                        hashers[name].update(arr.tobytes())
+                    print(f"iteration={iteration}\tUpdating hashes took {time.time() - hash_update_start} seconds.")
+                    
+                    flush_consolidate_start = time.time()
+                    zarr.consolidate_metadata(zroot.store)
+                    # Once again a dirty hack to force flushing...
+                    # Zarr v3 offers zroot.flush() but requires python>=3.11 but isaac lab python=3.10...
+                    for dirpath, _, filenames in os.walk(zroot.path):
+                        for fname in filenames:
+                            fpath = os.path.join(dirpath, fname)
+                            with open(fpath, 'rb') as f:
+                                os.fsync(f.fileno())
+                    print(f"zroot flush and metadata consolidation took {(time.time() - flush_consolidate_start):.4f} seconds.")
+
+                    # Use compare_tensor_snapshots.py for torch.allclose between two runs because rounding may influence the hash
+                    for name, h in hashers.items():
+                        print(f"[snapshot @{global_step}] hash({name}) = {h.hexdigest()}")
 
         # flatten the batch
         b_obs = obs.reshape((-1,) + SINGLE_OBSERVATION_SPACE)
