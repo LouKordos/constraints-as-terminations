8a64020df03fd508a719df96d35fb57b0a6d6b63
Loukas Kordos <loukas.kordos@uni-tuebingen.de>
2025-06-05 15:21:26 +0200

    Archive to downloads instead

--- archive-logs-dir.sh
+++ archive-logs-dir.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 set -eux pipefail
 TOTAL_BYTES=$(du -sb ./logs/clean_rl/ | cut -f1)
-time tar -cf - ./logs/clean_rl/ | pv -s ${TOTAL_BYTES} | zstd --ultra -22 -T0 -o constraints-as-terminations-logs-and-runs.tar.zst
+time tar -cf - ./logs/clean_rl/ | pv -s ${TOTAL_BYTES} | zstd --ultra -22 -T0 -o $/HOME/Downloads/constraints-as-terminations-logs-and-runs.tar.zst
 
 # To install: wget -qO- https://github.com/Blutsh/Swish/releases/download/1.0.1/swish-1.0.1-x86_64-unknown-linux-musl.tar.gz | tar xz --strip-components=1 -C ~/.local/bin/ --wildcards '*/swish'
-swish constraints-as-terminations-logs-and-runs.tar.zst
+swish $HOME/Downloads/constraints-as-terminations-logs-and-runs.tar.zst
