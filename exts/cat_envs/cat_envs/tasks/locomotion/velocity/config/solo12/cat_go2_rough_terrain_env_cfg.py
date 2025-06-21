57212d4da3fcb2d4bf5c4a7ac4343da7ebfd5ad1
Loukas Kordos <loukas.kordos@uni-tuebingen.de>
2025-06-21 10:35:59 +0200

    Reduce initial terrain level during training

--- exts/cat_envs/cat_envs/tasks/locomotion/velocity/config/solo12/cat_go2_rough_terrain_env_cfg.py
+++ exts/cat_envs/cat_envs/tasks/locomotion/velocity/config/solo12/cat_go2_rough_terrain_env_cfg.py
@@ -138,7 +138,7 @@ class MySceneCfg(InteractiveSceneCfg):
         prim_path="/World/ground",
         terrain_type="generator",
         terrain_generator=seeded_rough_cfg,
-        max_init_terrain_level=5,
+        max_init_terrain_level=1,
         collision_group=-1,
         physics_material=sim_utils.RigidBodyMaterialCfg(
             friction_combine_mode="multiply",
