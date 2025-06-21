6150de915c8f82e2f8dd8eefc36e08494f6cdca5
Loukas Kordos <loukas.kordos@uni-tuebingen.de>
2025-06-21 18:30:42 +0200

    [Eval] Reduce height offset for foot tip

--- scripts/eval.py
+++ scripts/eval.py
@@ -461,7 +461,7 @@ def main():
         foot_positions_body_frame_buffer.append(foot_positions_body)
 
         # Calculate foot height above ground using raycaster sensor in each foot (sole/contact frame)
-        foot_com_toe_tip_offset = 0.023 # This makes swing height more intuitive, without the offset, standing still reports a ~0.0234m swing height
+        foot_com_toe_tip_offset = 0.0228 # This makes swing height more intuitive, without the offset, standing still reports a positive stance height
         terrain_offset_feet = np.array([[0, 0, env.unwrapped.scene[f"ray_caster_{link_name}"].data.ray_hits_w[:, 0, 2].cpu().item() + foot_com_toe_tip_offset] for link_name in foot_links])
         # print(f"terrain_z_feet={terrain_offset_feet}")
         foot_positions_contact_frame = foot_positions_world - terrain_offset_feet
