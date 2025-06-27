161c861e14b4ca7aa0034309a490e21276acc013
olympus-fedora <git.loukas@kordos.de>
2025-06-27 13:45:20 +0200

    Add skeleton

---
+++ sim2real/build-and-run.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+set -exo pipefail
+
+# Default build type
+BUILD_TYPE="Release"
+BUILD_DIR="/app/build"
+LOG_FILE="/app/build.log"
+export CLICOLOR=1
+export CLICOLOR_FORCE=1
+
+# Parse command line arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --build-type=*)
+            BUILD_TYPE="${1#*=}"
+            shift
+            ;;
+        --build-type)
+            BUILD_TYPE="$2"
+            shift 2
+            ;;
+        *)
+            echo "Unknown parameter passed: $1, exiting."
+            exit 1
+            ;;
+    esac
+done
+        
+if [[ -z "${DOCKER_FLAG_FOR_RUN_SCRIPT}" ]]; then
+    echo "Host (no docker) detected."
+    docker compose --progress plain up -d
+    echo "Docker container is now running, starting interactive shell. Run /app/build-and-run.sh inside the shell to proceed."
+    docker exec -it force-controlled-switch-assembly-force-controlled-switch-assembly-1 /bin/bash
+else
+    echo "Docker detected."
+    cd /app
+    time cmake -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" -S . -B ${BUILD_DIR}
+    if [[ ! -f "/tracy-for-capture-built.marker" ]]; then
+        echo "-----------------------------------------Setting up automatic tracy profile capture------------------------------------------------"
+        cp -r ${BUILD_DIR}/_deps/tracy-src/ /tracy-for-capture/
+        rm -r /tracy-for-capture/capture/build || true
+        cmake -B /tracy-for-capture/capture/build -S /tracy-for-capture/capture -DCMAKE_BUILD_TYPE=Release -DNO_FILESELECTOR=ON
+        cmake --build /tracy-for-capture/capture/build/ --config Release --parallel
+        touch "/tracy-for-capture-built.marker"
+        echo "-------------------------------------------------Tracy capture setup complete------------------------------------------------------"
+    fi
+    pkill -f tracy-capture || echo "No tracy capture instances running."
+    TRACY_LOG_FILE="/app/logs/tracy-capture-$(date '+%Y-%m-%d-%H-%M-%S').log"
+    # Start a new tracy-capture instance and redirect output to the log file 
+    /tracy-for-capture/capture/build/tracy-capture -o /app/tracy-profiles/$(date '+%Y-%m-%d-%H-%M-%S').tracy > "$TRACY_LOG_FILE" 2>&1 & 
+    TRACY_PID=$!
+    echo "Tracy capture started with PID $TRACY_PID. Logs are being written to $TRACY_LOG_FILE"
+    
+    time cmake --build "$BUILD_DIR" -j $(nproc) -v 2>&1 | tee "$LOG_FILE"
+    echo "-----------------------------------------------------------------------------------------------------------------------------------"
+    # Check for warnings in the build output
+    if grep -q "warning:" "$LOG_FILE"; then
+        echo -e "\n\033[1;33mWarnings detected during compilation, waiting for confirmation.\033[0m"
+        read
+    else
+        echo -e "\n\033[1;32mNo warnings detected during compilation.\033[0m"
+    fi
+
+    if [[ "${BUILD_TYPE}" = "Debug" ]]; then
+        gdb ${BUILD_DIR}/src/pick_and_place
+    else
+        ${BUILD_DIR}/src/pick_and_place
+    fi
+fi
