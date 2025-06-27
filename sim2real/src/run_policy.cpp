77757d5cebb4a9b7ef50559b8cd6173e49263e54
olympus-fedora <git.loukas@kordos.de>
2025-06-27 20:39:37 +0200

    Install and test libtorch

--- sim2real/src/run_policy.cpp
+++ sim2real/src/run_policy.cpp
@@ -1,4 +1,16 @@
-int main()
+#include <print>
+#include <iostream>
+#include <torch/script.h>
+#include <memory>
+
+int main(int argc, const char *argv[])
 {
+    std::vector<torch::jit::IValue> inputs;
+    inputs.push_back(torch::ones({1, 3, 224, 224}));
+
+    at::Tensor test = torch::ones({3, 3, 3});
+
+    std::cout << test << std::endl;
+
     return 0;
 }
---
+++ sim2real/src/test_pytorch_policy.cpp
@@ -0,0 +1,4 @@
+int main()
+{
+    // TODO: Unit test that checks if policy produces the same outputs as python version with the same fixed inputs.
+}
