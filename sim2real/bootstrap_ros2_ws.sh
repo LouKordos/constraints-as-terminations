63348b4ea73cf8e2f2df9a31c63ae920953b4957
Loukas Kordos <loukas.kordos@uni-tuebingen.de>
2025-07-15 20:32:46 +0200

    Add alternative go2_odometry ROS workspace

--- sim2real/bootstrap_ros2_ws.sh
+++ sim2real/bootstrap_ros2_ws.sh
@@ -5,6 +5,15 @@ source /opt/ros/$ROS_DISTRO/setup.bash
 set -x # After verbose source
 
 BASE_DIR=/app # In case you want to run outside docker
+
+# For alternative odom, this will conflict with go2_bringup so do not run both at the time!:
+mkdir -p $BASE_DIR/odom_alternative_ws/src
+sudo chmod -R 777 $BASE_DIR/odom_alternative_ws
+cd $BASE_DIR/odom_alternative_ws/src
+git clone https://github.com/inria-paris-robotics-lab/go2_odometry.git
+git clone https://github.com/inria-paris-robotics-lab/go2_description.git
+git clone https://github.com/Unitree-Go2-Robot/unitree_go.git
+
 mkdir -p $BASE_DIR/ros2_ws/src/third_party # Use third_party for any external packages because it's ignored by git
 sudo chmod -R 777 $BASE_DIR/ros2_ws
 cd $BASE_DIR/ros2_ws/src/third_party # For custom code, use ros2_ws/src/
@@ -29,7 +38,13 @@ else
 fi
 
 rosdep install --from-paths src --ignore-src -r -y
-sudo apt-get install -y libyaml-cpp-dev libboost-all-dev ros-$ROS_DISTRO-realsense2-camera ros-$ROS_DISTRO-pointcloud-to-laserscan
+
+if [[ -d "${BASE_DIR}/odom_alternative_ws" ]]; then
+    rosdep install --from-paths $BASE_DIR/odom_alternative_ws/src --ignore-src -r -y
+    cd $BASE_DIR/odom_alternative_ws
+    colcon build
+    cd $BASE_DIR/ros2_ws
+fi
 
 FIRST_BUILD_MARKER=/colcon-ros2_ws_clean_build.marker
 COLCON_ARGS=() # To prevent duplication
