979fca0399672a3c0ebe891485d801851f4f8649
Loukas Kordos <loukas.kordos@uni-tuebingen.de>
2025-07-17 19:33:18 +0200

    Fix paths for odom workspace

--- sim2real/bootstrap_ros2_ws.sh
+++ sim2real/bootstrap_ros2_ws.sh
@@ -7,13 +7,13 @@ set -x # After verbose source
 BASE_DIR=/app # In case you want to run outside docker
 
 # For alternative odom, this will conflict with go2_bringup so do not run both at the time!:
-mkdir -p $BASE_DIR/odom_alternative_ws/src
-cd $BASE_DIR/odom_alternative_ws/src
-git clone https://github.com/inria-paris-robotics-lab/go2_odometry.git
-git clone https://github.com/inria-paris-robotics-lab/go2_description.git
-git clone https://github.com/Unitree-Go2-Robot/unitree_go.git
+mkdir -p $BASE_DIR/odom_alternative_ws/src/third_party/
+cd $BASE_DIR/odom_alternative_ws/src/third_party
+git clone https://github.com/inria-paris-robotics-lab/go2_odometry.git || (cd go2_odometry && git pull)
+git clone https://github.com/inria-paris-robotics-lab/go2_description.git || (cd go2_description && git pull)
+git clone https://github.com/Unitree-Go2-Robot/unitree_go.git || (cd unitree_go && git pull)
 # Lower foot contact threshold to finish initialization correctly
-sed -i "s|if np.min(f_contact) > 30|if np.min(f_contact) > 20|" src/go2_odometry/scripts/feet_to_odom_inekf.py
+sed -i "s|if np.min(f_contact) > 30|if np.min(f_contact) > 20|" ./go2_odometry/scripts/feet_to_odom_inekf.py
 
 mkdir -p $BASE_DIR/ros2_ws/src/third_party # Use third_party for any external packages because it's ignored by git
 cd $BASE_DIR/ros2_ws/src/third_party # For custom code, use ros2_ws/src/
@@ -23,7 +23,7 @@ cd $BASE_DIR/ros2_ws/src/third_party # For custom code, use ros2_ws/src/
 git clone -b humble https://github.com/Unitree-Go2-Robot/go2_robot.git go2_robot || (cd go2_robot && git pull)
 sed -i 's|https://github.com/Unitree-Go2-Robot/go2_driver.git|https://github.com/LouKordos/go2_driver.git|' go2_robot/dependencies.repos # Replacement with fork only needed until PR is merged
 vcs import < go2_robot/dependencies.repos
-git clone https://github.com/Ericsii/livox_ros_driver2/tree/feature/use-standard-unit || (cd livox_ros2_driver2 && git pull)
+git clone https://github.com/Ericsii/livox_ros_driver2 -b feature/use-standard-unit || (cd livox_ros_driver2 && git pull)
 git clone --recurse-submodules https://github.com/LouKordos/FAST_LIO_ROS2.git || (cd FAST_LIO_ROS2.git && git pull)
 git clone https://github.com/LouKordos/LiDAR_IMU_Init.git/ || (cd LiDAR_IMU_Init && git pull)
 git clone --recurse-submodules https://github.com/HesaiTechnology/HesaiLidar_ROS_2.0.git HesaiLidar_ROS_2.0 || (cd HesaiLidar_ROS_2.0 && git pull)
