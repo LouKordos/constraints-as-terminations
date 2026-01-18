ef2a0103f0260352eb2fb2868b2e7c7772773462
Loukas Kordos <loukas.kordos@uni-tuebingen.de>
2026-01-18 10:14:48 +0100

    Formatting

--- sim2real/odom_alternative_ws/src/go2_livox_bringup/launch/livox.launch.py
+++ sim2real/odom_alternative_ws/src/go2_livox_bringup/launch/livox.launch.py
@@ -14,11 +14,11 @@ def generate_launch_description():
         # Update these arguments with your measured physical offsets!
         # args: 'x y z yaw pitch roll parent_frame child_frame' (meters and radians)
         Node(                                                                                                                                                                            
-            package='tf2_ros', 
-            executable='static_transform_publisher', 
-            name='base_to_livox_static_transform', 
-            arguments=['0.34', '0.0', '0.15', '0', '0.77', '0', 'base', 'livox_frame'], 
-            output='screen', 
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='base_to_livox_static_transform',
+            arguments=['0.34', '0.0', '0.15', '0', '0.77', '0', 'base', 'livox_frame'],
+            output='screen',
         ),
 
         # Livox MID360 Driver Node
