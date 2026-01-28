5c27a0cd7b09a6f51e64614e69aa32d0d78287a3
Loukas Kordos <loukas.kordos@uni-tuebingen.de>
2026-01-28 19:13:25 +0100

    Add option for replacing go2_odometry with vicon motion tracking

--- sim2real/odom_alternative_ws/src/go2_livox_bringup/launch/bringup.launch.py
+++ sim2real/odom_alternative_ws/src/go2_livox_bringup/launch/bringup.launch.py
@@ -1,8 +1,11 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import PathJoinSubstitution
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
 from launch_ros.substitutions import FindPackageShare
+from launch.conditions import IfCondition, UnlessCondition
+from launch_ros.actions import Node
+from go2_description import GO2_DESCRIPTION_URDF_PATH
 
 def generate_launch_description():
     go2_odometry_launch_file = PathJoinSubstitution(
@@ -13,12 +16,48 @@ def generate_launch_description():
         [FindPackageShare("go2_livox_bringup"), "launch", "livox.launch.py"]
     )
 
-    return LaunchDescription([
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([go2_odometry_launch_file]),
-        ),
+    with open(GO2_DESCRIPTION_URDF_PATH, "r") as info:
+        robot_desc = info.read()
 
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([livox_launch_file])
-        ),
+    use_vicon_arg = DeclareLaunchArgument(
+        "use_vicon",
+        default_value="false",
+        description="Set to true if you want to use vicon tracking instead of go2_odometry. This simply adjusts the static transform being published for the lidar and starts only the robot state publisher instead of full go2_odometry."
+    )
+    use_vicon = LaunchConfiguration("use_vicon")
+
+    livox_include = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([livox_launch_file]),
+        launch_arguments={"use_vicon": use_vicon}.items()
+    )
+    odometry_include = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([go2_odometry_launch_file]),
+        condition=UnlessCondition(use_vicon)
+    )
+
+    state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        output="screen",
+        parameters=[{"robot_description": robot_desc}],
+        arguments=[GO2_DESCRIPTION_URDF_PATH],
+        condition=IfCondition(use_vicon)
+    )
+
+    state_converter_node = Node(
+        package="go2_odometry",
+        executable="state_converter_node",
+        name="state_converter_node",
+        parameters=[],
+        output="screen",
+        condition=IfCondition(use_vicon)
+    )
+
+    return LaunchDescription([
+        use_vicon_arg,
+        livox_include,
+        odometry_include,
+        state_publisher_node,
+        state_converter_node
     ])
--- sim2real/odom_alternative_ws/src/go2_livox_bringup/launch/livox.launch.py
+++ sim2real/odom_alternative_ws/src/go2_livox_bringup/launch/livox.launch.py
@@ -1,36 +1,80 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch.conditions import IfCondition, UnlessCondition
 from launch_ros.actions import Node
 
 def generate_launch_description():
     # Path to the Livox driver's config file
-    livox_config_path = os.path.join(
-        get_package_share_directory('livox_ros_driver2'), 'config', 'MID360_config.json'
+    livox_config_path = os.path.join(get_package_share_directory('livox_ros_driver2'), 'config', 'MID360_config.json')
+
+    use_vicon_arg = DeclareLaunchArgument(
+        "use_vicon",
+        default_value="true",
+        description="Decides which frame names to use for the static transform between lidar and base frame"
+    )
+    use_vicon = LaunchConfiguration("use_vicon")
+
+    # Static transform from base to the livox_frame
+    # Update these arguments with your measured physical offsets!
+    # args: 'x y z yaw pitch roll parent_frame child_frame' (meters and radians)
+    x_offset = 0.34
+    y_offset = 0.0
+    z_offset = 0.155
+    roll_offset = 0.0
+    pitch_offset = 0.784
+    yaw_offset = 0.0
+    vicon_base_frame = "vicon/Go2_Loukas/Go2"
+    odometry_base_frame = "base"
+
+    tf_vicon_node = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name='base_to_livox_static_transform',
+        arguments=[str(x_offset), str(y_offset), str(z_offset), str(yaw_offset), str(pitch_offset), str(roll_offset), vicon_base_frame, 'livox_frame'],
+        output='screen',
+        condition=IfCondition(use_vicon)
+    )
+
+    # This is needed so that the state publisher and the URDF find a transform for the base.
+    # Vicon publishes vicon_base_frame as the base so we just need a transform with base in same position as vicon frame
+    tf_vicon_base_node = Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        name="base_to_vicon_static_transform",
+        arguments=["0","0","0","0","0","0", vicon_base_frame, odometry_base_frame],
+        output="screen",
+        condition=IfCondition(use_vicon)
+    )
+
+    tf_odometry_node = Node( 
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name='base_to_livox_static_transform',
+        arguments=[str(x_offset), str(y_offset), str(z_offset), str(yaw_offset), str(pitch_offset), str(roll_offset), odometry_base_frame, 'livox_frame'],
+        output='screen',
+        condition=UnlessCondition(use_vicon)
+    )
+    
+    livox_driver_node = Node(
+        package='livox_ros_driver2',
+        executable='livox_ros_driver2_node',
+        name='livox_lidar_publisher',
+        output='screen',
+        parameters=[
+            {'xfer_format': 0},
+            {'publish_freq': 30.0},
+            {'frame_id': 'livox_frame'},
+            {'user_config_path': livox_config_path}
+        ]
     )
 
     return LaunchDescription([
-        # Static transform from base to the livox_frame
-        # Update these arguments with your measured physical offsets!
-        # args: 'x y z yaw pitch roll parent_frame child_frame' (meters and radians)
-        Node(                                                                                                                                                                            
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            name='base_to_livox_static_transform',
-            arguments=['0.34', '0.0', '0.15', '0', '0.77', '0', 'base', 'livox_frame'],
-            output='screen',
-        ),
-
-        # Livox MID360 Driver Node
-        Node(
-            package='livox_ros_driver2',
-            executable='livox_ros_driver2_node',
-            name='livox_lidar_publisher',
-            output='screen',
-            parameters=[
-                {'xfer_format': 0},
-                {'frame_id': 'livox_frame'},
-                {'user_config_path': livox_config_path}
-            ]
-        ),
+        use_vicon_arg,
+        tf_vicon_node,
+        tf_vicon_base_node,
+        tf_odometry_node,
+        livox_driver_node
     ])
