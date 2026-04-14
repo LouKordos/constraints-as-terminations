d166a46a8d9a6163f7865317799758f2b2606e37
Loukas Kordos <loukas.kordos@uni-tuebingen.de>
2026-04-14 17:34:32 +0200

    Test lowstate subscription

--- sim2real/ros2_ws/src/cat_controller/src/cat_control_node.cpp
+++ sim2real/ros2_ws/src/cat_controller/src/cat_control_node.cpp
@@ -1,22 +1,32 @@
 #include <chrono>
+#include <cmath>
+#include <thread>
 #include <memory>
 #include <string>
+#include <functional>
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+
+// #include "unitree_go/msg/sport_mode_state.hpp"
+#include "unitree_go/msg/low_state.hpp"
+#include "unitree_go/msg/low_cmd.hpp"
+// #include "cat_controller/motor_crc.h" // Copied from go2 repo because its needed for sending valid motor commands and they do not install these header files automatically
 
 using namespace std::chrono_literals;
 
 class CaTControlNode : public rclcpp::Node {
     public:
         CaTControlNode() : Node("cat_control_node") {
-            auto timer_callback = [this]() -> void {
-                RCLCPP_INFO(this->get_logger(), "Hello world");
-            };
-            timer_ = this->create_wall_timer(500ms, timer_callback);
+            robot_state_timer_ = this->create_wall_timer(2000ms, std::bind(&CaTControlNode::read_robot_state, this));
         }
     private:
-        rclcpp::TimerBase::SharedPtr timer_;
+        void read_robot_state() {
+            RCLCPP_INFO(this->get_logger(), "Test");
+        }
+
+        rclcpp::TimerBase::SharedPtr robot_state_timer_;
+        rclcpp::Subscription<unitree_go::msg::LowState>::SharedPtr robot_state_sub_;
 };
 
 int main(int argc, char* argv[]) {
