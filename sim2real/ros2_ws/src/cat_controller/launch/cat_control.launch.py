fbb8eb72604c0d5ef30e297b82eb0b8a719afc48
Loukas Kordos <loukas.kordos@uni-tuebingen.de>
2026-04-17 09:56:23 +0200

    Add launchfile for controller

---
+++ sim2real/ros2_ws/src/cat_controller/launch/cat_control.launch.py
@@ -0,0 +1,58 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import RegisterEventHandler, ExecuteProcess, LogInfo, DeclareLaunchArgument
+from launch.event_handlers import OnProcessExit
+from launch.substitutions import LaunchConfiguration, PythonExpression
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    config_path = os.path.join(get_package_share_directory("cat_controller"), "config", "cat_control_node.yaml")
+
+    use_vicon_arg = DeclareLaunchArgument(
+        "use_vicon",
+        default_value="false",
+        description="Determines which state topic to wait for."
+    )
+    use_vicon = LaunchConfiguration("use_vicon")
+
+    starting_log = LogInfo(
+        msg="[HEALTH CHECK] Waiting for Odometry/Vicon and Livox topics to publish data..."
+    )
+    odom_topic = PythonExpression([
+        "'vicon/Go2_Loukas/Go2_Loukas/pose' if '", use_vicon, "' == 'true' else '/odometry/filtered'"
+    ])
+
+    health_check_cmd = ExecuteProcess(
+        cmd=["sh", "-c", [
+            "ros2 topic echo --once /lowstate > /dev/null && "
+            "ros2 topic echo --once ", odom_topic, " > /dev/null && ",
+            "ros2 topic echo --once /livox/lidar > /dev/null"
+        ]],
+        output='log'
+    )
+
+    cat_control_node = Node(
+        package="cat_controller",
+        executable="cat_control_node",
+        name="cat_control_node",
+        output="screen",
+        parameters=[config_path]
+    )
+
+    start_controller_event = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=health_check_cmd,
+            on_exit=[
+                LogInfo(msg="[HEALTH CHECK PASSED] All sensors active. Starting CaT Control Node..."),
+                cat_control_node
+            ]
+        )
+    )
+
+    return LaunchDescription([
+        use_vicon_arg,
+        starting_log,
+        health_check_cmd,
+        start_controller_event
+    ])
