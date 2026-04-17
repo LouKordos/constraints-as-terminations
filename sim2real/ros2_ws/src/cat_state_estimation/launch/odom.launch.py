9ea064cfd558317a642b6d8b467c3bad179cba4d
Loukas Kordos <loukas.kordos@uni-tuebingen.de>
2026-04-17 10:59:03 +0200

    Add missing import

--- sim2real/ros2_ws/src/cat_state_estimation/launch/odom.launch.py
+++ sim2real/ros2_ws/src/cat_state_estimation/launch/odom.launch.py
@@ -1,6 +1,6 @@
 import os
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 from launch.launch_description_sources import PythonLaunchDescriptionSource
