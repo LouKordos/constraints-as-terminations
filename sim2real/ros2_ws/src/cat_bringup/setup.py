f2405620722828cffc498651bdd9aeb799f75ef1
Loukas Kordos <loukas.kordos@uni-tuebingen.de>
2026-04-18 14:11:40 +0200

    Fix missing comma

--- sim2real/ros2_ws/src/cat_bringup/setup.py
+++ sim2real/ros2_ws/src/cat_bringup/setup.py
@@ -12,8 +12,8 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob.glob(os.path.join('launch', '*.launch.py'))),
-        (os.path.join('share', package_name, 'config'), glob.glob(os.path.join('config', '*.yaml')))
-        (os.path.join('share', package_name, 'rviz'), glob.glob(os.path.join('rviz', '*')))
+        (os.path.join('share', package_name, 'config'), glob.glob(os.path.join('config', '*.yaml'))),
+        (os.path.join('share', package_name, 'rviz'), glob.glob(os.path.join('rviz', '*.rviz')))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
