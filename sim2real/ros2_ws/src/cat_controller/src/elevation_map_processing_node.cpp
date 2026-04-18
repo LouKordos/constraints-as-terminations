c6261fa1f2057c7169f2d6cc10da1568cfc22c72
Loukas Kordos <loukas.kordos@uni-tuebingen.de>
2026-04-18 17:49:32 +0200

    Combine processor and node, logic is less complex than expected

--- sim2real/ros2_ws/src/cat_controller/src/elevation_map_processing_node.cpp
+++ sim2real/ros2_ws/src/cat_controller/src/elevation_map_processing_node.cpp
@@ -16,17 +16,24 @@ public:
     }
 
 private:
-    void publish_processed_map()
+    void sample_and_publish_processed_map()
     {
         // TODO: Load global atomic map, do NOT copy just use const &
         // TODO: Probably keep private working copy of current_procesed_map and call reserve in constructor on that to avoid heap allocs? Downside is
         // thread safety but since node is self contained it should be fine
         // TODO: Call map processor to generate processed map
         // TODO: Create custom msg, publish, log
+
+        // Takes a GridMap and applies required transformation and interpolation to prepare it for policy inference
+        // TODO: Decide if integrating ros is bad because it introduces a huge dependency, benefit is that I can just pass a ros data structure for
+        // pose
+        // TODO: gridmap heavily relies on exceptions, make sure to fully catch all of them, log and return only std::expected, NO EXCEPTIONS ALLOWED
+        // IN THIS CLASS!!! Proboably use IsInside to avoid try catch block around hot loop of ~150 lookups?
+        // TODO: If I decouple this from Ros I need a custom data type for pose
     }
 
     // How can I improve naming here?
-    void map_subscriber_callback(const cat_perception_msgs::msg::ProcessedElevationMap msg)
+    void source_map_subscriber_callback(const grid_map_msgs::msg::GridMap & msg)
     {
         // TODO: Check callback age using helper!
         // TODO: Convert elevation map to grid_map type
