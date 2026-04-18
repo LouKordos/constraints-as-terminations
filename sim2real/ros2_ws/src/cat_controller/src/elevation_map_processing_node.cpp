eb991834a027fd9d7462bbaa556bca4ac5da9434
Loukas Kordos <loukas.kordos@uni-tuebingen.de>
2026-04-18 18:46:54 +0200

    Update TODO

--- sim2real/ros2_ws/src/cat_controller/src/elevation_map_processing_node.cpp
+++ sim2real/ros2_ws/src/cat_controller/src/elevation_map_processing_node.cpp
@@ -19,25 +19,25 @@ private:
     void sample_and_publish_processed_map()
     {
         // TODO: Load global atomic map, do NOT copy just use const &
+        // TODO: Check global threadsafe elevation map age, exit if too old
         // TODO: Probably keep private working copy of current_procesed_map and call reserve in constructor on that to avoid heap allocs? Downside is
         // thread safety but since node is self contained it should be fine
-        // TODO: Call map processor to generate processed map
+        // process map to convert into polciy observation format as in python
         // TODO: Create custom msg, publish, log
 
-        // Takes a GridMap and applies required transformation and interpolation to prepare it for policy inference
-        // TODO: Decide if integrating ros is bad because it introduces a huge dependency, benefit is that I can just pass a ros data structure for
-        // pose
-        // TODO: gridmap heavily relies on exceptions, make sure to fully catch all of them, log and return only std::expected, NO EXCEPTIONS ALLOWED
-        // IN THIS CLASS!!! Proboably use IsInside to avoid try catch block around hot loop of ~150 lookups?
-        // TODO: If I decouple this from Ros I need a custom data type for pose
+        // TODO: for interpolation, first implement a manual bilinear interpolation that does the same as python
+        // Keep this TODO: Test with grid_map builtin interpolation and check the differences because that is cleaner. I don't mind using nearest
+        // neighbor since invalid cells are very rare in my use case anyway
+
+        // TODO: gridmap heavily relies on exceptions, make sure to fully catch all of them, log and return only std::expected, NODE SHOULD NOT THROW
+        // EXCEPTIONS BUT HANDLE USING shutdown_coordinator instead! Proboably use IsInside to avoid try catch block around hot loop of ~150 lookups?
+
+        // TODO: After confirmed working, start simplifying and cleaning up until there are differences when running the rosbag-checker on it
     }
 
-    // How can I improve naming here?
     void source_map_subscriber_callback(const grid_map_msgs::msg::GridMap & msg)
     {
-        // TODO: Check callback age using helper!
-        // TODO: Convert elevation map to grid_map type
-        // TODO: store globally using atomic shared ptr probably, need to check best way here
+        // TODO: store globally using atomic shared ptr probably, need to check best approach here to avoid expensive copies and heap allocs here.
         // TODO: Log info
     }
 
