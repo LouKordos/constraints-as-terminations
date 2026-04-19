e2f7200a46be231108b8d3359e22386dc61174f0
Loukas Kordos <loukas.kordos@uni-tuebingen.de>
2026-04-19 19:25:49 +0200

    Explain concurrency model further

--- sim2real/ros2_ws/src/cat_controller/src/elevation_map_processing_node.cpp
+++ sim2real/ros2_ws/src/cat_controller/src/elevation_map_processing_node.cpp
@@ -133,7 +133,7 @@ private:
         // We bypass locking by allocating a new object on the heap, converting the message without locking yet, and only then atomically store the
         // new pointer globally. That way, the high-frequency timer thread never blocks waiting for a deep copy to finish.
         // I am aware tat atomic shared ptrs are not lock-free, but as the control node will notie latency spikes that might occur due to the heap
-        // allocs or waiting for a lock here, this is acceptable.
+        // allocs or waiting for a lock here, this is acceptable. If it becomes a real bottleneck, I will switch to double buffering.
         std::shared_ptr<grid_map::GridMap> latest_map = global_grid_map_.load();
         // Always need to check for null pointer, e.g. for scenario when no map has been received yet.
         if (!latest_map) {
