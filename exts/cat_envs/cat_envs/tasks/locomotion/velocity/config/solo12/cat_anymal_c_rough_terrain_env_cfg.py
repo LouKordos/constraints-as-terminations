345ad2a5dc4b2d382d765bafa9f6636677dd294a
Loukas Kordos <loukas.kordos@uni-tuebingen.de>
2026-08-11 11:06:41 +0200

    Adjust action rate constraint limit due to action scale

--- exts/cat_envs/cat_envs/tasks/locomotion/velocity/config/solo12/cat_anymal_c_rough_terrain_env_cfg.py
+++ exts/cat_envs/cat_envs/tasks/locomotion/velocity/config/solo12/cat_anymal_c_rough_terrain_env_cfg.py
@@ -84,7 +84,11 @@ class AnymalCRoughTerrainEnvCfg(Go2RoughTerrainEnvCfg):
         self.constraints.joint_torque.params["limit"] = 80.0
         self.constraints.joint_velocity.params["limit"] = 12.0
         self.constraints.joint_acceleration.params["limit"] = 600.0
-        self.constraints.action_rate.params["limit"] = 80.0
+        self.constraints.joint_acceleration.params["limit"] = 800.0
+        # Action rate depends on action scale, so lower action rate for ANYMAL makes this constraint more restrictive!
+        # Thus we scale by the ratio of original Go2 action scale 0.8/0.5=1.6
+        # THIS MIGHT NEED ADJUSTMENTS BECAUSE ACTION RATE COULD BE TOO HIGH
+        self.constraints.action_rate.params["limit"] = 80.0 * 1.6
 
         self.constraints.contact.params["names"] = ["base", ".*_THIGH"]
         self.constraints.foot_contact_force.params.update(limit=1000.0, names=[".*_FOOT"])
